d3b054c8694d706bf4f8465b6a26190870f397ea
Kaylee haynes <kayleehaynes@yahoo.com>
2020-03-19 21:46:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson";
 
 var greetSpecialGuest = greet + specialGuest + "!";
 var letsTalk = "Let\'s talk about "
-var topic = "space";
+var topic = " space";
 
-var conversation = "Let\'s talk about " + `${topic}`;
+var conversation = "Let\'s talk about" + `${topic}` + ".";
