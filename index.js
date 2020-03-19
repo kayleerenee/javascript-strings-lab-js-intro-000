542a0d8266d71d77a64773b48edd119ac5f89743
Kaylee haynes <kayleehaynes@yahoo.com>
2020-03-19 21:17:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var greeting = "Hello, everybody!";
+var greet = "Hello, "
+var specialGuest = "Neil deGrasse Tyson";
 
-var specialGuest = "Neil deGrasse Tyson!";
-
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = greet + specialGuest + "!";
 
 var topic = "Let's talk about space.";
 
